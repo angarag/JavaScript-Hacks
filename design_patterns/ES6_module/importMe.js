3e469bc45a77e2f9a784f54a5bf6dd8aee381c66
angarag <mars0411@gmail.com>
2019-05-25 17:12:22 -0500

    design pattern etc added

---
+++ design_patterns/ES6_module/importMe.js
@@ -0,0 +1,7 @@
+function sum(a, b) {
+  return a + b;
+}
+function pow(a, b) {
+  return Math.pow(a, b);
+}
+export { sum, pow };
---
+++ design_patterns/ES6_module/module.js
@@ -0,0 +1,10 @@
+/*ES6
+import * as utils from "./importMe.js";
+import { sum as toplam, pow as power } from "./importMe.js";
+
+console.log(toplam(4, 5));
+console.log(power(2, 10));
+*/
+//pre-ES6
+var test = require("./importMe");
+console.log(test);

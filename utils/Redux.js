7e6c5543b197391d8b176c35b3b5c91fdab5e378
angarag <mars0411@gmail.com>
2019-05-30 22:23:29 -0500

    redux reducer func added

--- utils/Redux.js
+++ utils/Redux.js
@@ -21,3 +21,17 @@ var store = Redux.createStore(favoriteColors);
 store.dispath(addColor("blue"));
 store.dispath(removeColor("blue"));
 console.log(store.getState());
+
+function counter(state, action) {
+  if (state == undefined) return { count: 0 };
+  var count = state.count;
+  switch (action.type) {
+    case "increase":
+      return { count: count + 1 };
+    case "decrease":
+      return { count: count + 1 };
+    default:
+      return state;
+  }
+}
+export default counter;

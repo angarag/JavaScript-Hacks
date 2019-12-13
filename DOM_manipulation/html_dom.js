5e6e671deed9ee7bae94e412769de582b7ae8f60
Angarag <mars0411@gmail.com>
2019-12-13 14:07:02 -0600

    cookie getter setter methods added, token... added

--- DOM_manipulation/html_dom.js
+++ DOM_manipulation/html_dom.js
@@ -20,3 +20,44 @@ document.write(text)
 //Adding Event Handlers
 document.getElementById("demo").addEventListener("click", myFunction);
 document.getElementById(id).onclick = function(){code}
+
+function saveToken(token) {
+  localStorage.clear();
+  localStorage.setItem("token", token);
+  const base64Url = token.split(".")[1];
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  const jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map(function(c) {
+        return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+      })
+      .join("")
+  );
+  const jsObject = JSON.parse(jsonPayload);
+  localStorage.setItem("winNbr", jsObject.winNbr);
+  localStorage.setItem("token-payload-decoded", jsonPayload);
+  return jsObject;
+}
+function setCookie(name, value, days = 1, expire = null) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = "; expires=" + date.toUTCString();
+  } else if (expire) expires = "; expires=" + expire.toUTCString();
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+function eraseCookie(name) {
+  document.cookie = name + "=; Max-Age=-99999999;";
+}

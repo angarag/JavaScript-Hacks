f422dfa11e9e6765e7407b619ed1d46e3a57b342
Angarag <mars0411@gmail.com>
2020-01-07 22:54:01 -0600

    Object.keys vs for in loop for cloned obj

--- data_structures/object.js
+++ data_structures/object.js
@@ -20,3 +20,14 @@ class Person{
 }
 //Enhaced object properties
 let [x,y]={'a','b'}  === var x ='a'; var y='b';obj2={x:x,y:y}
+
+
+
+let d={};
+d.test=1;
+let c=Object.create(d);
+d.test=2;
+c.test==d.test;
+//Object keys will iterate over only own properties while for(let in) loop will do deep iteration.
+Object.keys(c) == for(let key in c);
+

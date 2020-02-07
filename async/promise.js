81c36582ef327335e86de63d0b8112531130c02a
Angarag <mars0411@gmail.com>
2020-02-07 15:31:32 -0600

    Update promise.js

--- async/promise.js
+++ async/promise.js
@@ -1,47 +1,20 @@
-
-const pp1 = new Promise(resolve => resolve("Angarag"));
-const pp2 = new Promise((_, reject) => reject("User does not exists!"));
-const pp3 = new Promise(resolve => resolve("Batjargal"));
-Promise.all([pp1, pp3, pp2])
-  .then(response => {
-    console.log("all fulfilled:", response);
-  })
-  .catch(error => console.error("not all fulfilled:", error));
-  
-Promise.allSettled([pp1, pp2, pp3])
-  .then(response => {
-    console.log("all with status and value:", response);
-  })
-
 const test = () => {
   console.log("when will I be printed");
 };
-const all = arr => {
+const all = async arr => {
   let result = [];
   console.log("this will be printed only after func call");
-  setTimeout(() => {
-    result.push(-1);
-  }, 0);
-  return helper(arr);
-  async function helper(arr) {
-    let temp = null;
-    console.log("main operation");
-    setTimeout(() => {
-      result.push(1111);
-    }, 0);
-    for (let i = 0; i < arr.length; i++) {
-      result.push(i);
-      console.log("before pushing result to array", result);
-      temp = await arr[i];
-      result.push(temp);
-      console.log("after pushing result to array", result);
-      if (i == arr.length - 1)
-        return new Promise(res => {
-          res(result);
-        });
-    }
-    console.log("main operation after loop");
+  // setTimeout(() => {
+  //   result.push(-1);
+  // }, 1);
+  for (let i = 0; i < arr.length; i++) {
+    //result.push(i);
+    //console.log("   before pushing result to array", result);
+    temp = await arr[i];
+    result.push(temp);
+    //console.log("   after pushing result to array", result);
   }
+  return result;
 };
 const p2 = new Promise(res => {
   console.log("pre p2");
@@ -57,10 +30,27 @@ const p1 = new Promise(res => {
 });
 const p3 = Promise.resolve("p3");
 all([p3, p1, p2, p3]).then(values => {
-  console.log(values);
+  console.log("Custom Promise all:", values);
 });
 
 console.log("regular Promise all beginning");
-Promise.all([p1, p2]).then(r => {
-  console.log(r);
+Promise.all([p3, p1, p2, p3]).then(r => {
+  console.log("Regular Promise all:", r);
+});
+
+const pp2 = new Promise(resolve => {
+  console.log("pp2");
+  setTimeout(() => {
+    resolve("Angarag");
+  }, 0);
+});
+const pp1 = new Promise((_, reject) => _("User does not exists!"));
+const pp3 = new Promise(resolve => resolve("Batjargal"));
+Promise.all([pp1, pp2, pp3])
+  .then(response => {
+    console.log("Regular Promise 2 all:", response);
+  })
+  .catch(error => console.error("not all fulfilled:", error));
+Promise.all([pp1, pp3]).then(r => {
+  console.log("Regular Promise all without setTimeout:", r);
 });

6c17ebdbd53ac7fc967da8fc853d602842a35414
Angarag <mars0411@gmail.com>
2020-04-22 22:03:58 -0500

    Update math.js

--- math_funcs/math.js
+++ math_funcs/math.js
@@ -4,6 +4,16 @@ Math.floor(Math.random() * 10); // returns a random integer from 0 to 9
 Math.round(4.7); // returns 5
 Math.round(4.4); // returns 4
 Math.pow(8, 2); // returns 64
+function power(x,y){
+     if(y===0){return 1}
+     else if (y%2 ===0){
+         return power(x,parseInt(y/2))*power(x,parseInt(y/2))
+     }else{
+          return x*power(x,parseInt(y/2))*power(x,parseInt(y/2))
+     }
+
+}
+
 Math.sqrt(64); // returns 8
 Math.abs(-4.7); // returns 4.7
 Math.ceil(4.4); // returns 5, rounded up
